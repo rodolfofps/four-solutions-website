5577556ef9c0d065bf41821bf5a84412184af261
Rodolfo Santos <rodolfofps@hotmail.com>
2025-10-19 15:29:09 -0300

    Update path sliderClientes

--- sliderClientes/script.js
+++ sliderClientes/script.js
@@ -2,18 +2,38 @@
 document.addEventListener("DOMContentLoaded", () => {
             const carousel = document.querySelector("#carousel");
 
+            const images = [
+                "assets/cliente1.png",
+                "assets/cliente2.png",
+                "assets/cliente3.png",
+                "assets/cliente4.png",
+                "assets/cliente5.png"
+];
+             
+
 // Criação de função para criar grupos de cartões   
             const createGroup = (isAriaHidden) => {
                 const group = document.createElement("div");
                 group.id = "group";
                 if (isAriaHidden) group.setAttribute("aria-hidden", "true");
 
-                for (let i = 1; i <= 6; i++) {
+                for (let i = 0; i < images.length; i++) {
                     const card = document.createElement("div");
                     card.id = "card";
-                    card.textContent = i;
+                    
+
+                    // Cria o elemento <img> e define suas propriedades
+                    const img = document.createElement("img");
+                    img.src = images[i];
+                    img.alt = `Imagem ${i + 1}`;
+                    img.loading = "lazy"; // melhora desempenho no carregamento
+
+                    // Adiciona a imagem ao cartão
+                    card.appendChild(img);
+
+                    // Adiciona o cartão ao grupo
                     group.appendChild(card);
-                }
+                }    
 //Chama a função para criar os grupos de cartões
                 return group;
             };
